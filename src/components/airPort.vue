7df055044774f4138957cab695c9aa309693583d
xiachuanyoujia <1243357202@qq.com>
2023-01-19 23:10:09 +0800

    加载热气球缆车， 四翼桨机模型，41：4：50

--- src/components/airPort.vue
+++ src/components/airPort.vue
@@ -21,7 +21,10 @@ const dom = ref()
 
 onMounted(() => {
     init()
-    addTestCube()
+    // addTestCube()
+    addAirort()
+    addLights()
+
     renderScene()
     addAxesHelper()
     window.addEventListener('resize', onWindowResize)
@@ -31,8 +34,6 @@ onMounted(() => {
 
 const init = () => {
     dom.value = document.getElementById('container')
-    console.log("dom", dom.value)
-    console.log(dom.value.offsetHeight)
     scene = new THREE.Scene()
     renderer = new THREE.WebGL1Renderer()
     renderer.setSize(dom.value.offsetWidth, dom.value.offsetHeight)
@@ -53,12 +54,59 @@ const init = () => {
 const addTestCube = () => {
     const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
     const boxMaterial = new THREE.MeshBasicMaterial({
-        color: 0x00ff00, 
+        color: 0x00ff00,
     })
     const cube = new Mesh(boxGeometry, boxMaterial)
     cube.position.set(1, 1, 1)
     scene.add(cube)
 }
+
+//添加模型
+const addAirort = () => {
+    //热气球缆车
+    let balloonCableCar = new Promise((resolve) => {
+        gltfLoader.load('/models/CesiumBalloon.glb', gltf => {
+            gltf.scene.traverse(child => {
+                if (child.isMesh) {
+                    child.material.alphaTest = 1
+                    child.material.side = THREE.DoubleSide;
+                }
+            })
+            resolve(gltf.scene);
+        })
+    })
+
+    //四翼桨机
+    let fourWingsPropeller = new Promise((resolve) => {
+        gltfLoader.load('/models/CesiumDrone.glb', gltf => {
+            gltf.scene.traverse(child => {
+                if (child.isMesh) {
+                    child.material.alphaTest = 1
+                    child.material.side = THREE.DoubleSide;
+                }
+            })
+            resolve(gltf.scene);
+        })
+    });
+
+    Promise.all([balloonCableCar, fourWingsPropeller]).then((resolve) => {
+        scene.add(resolve[0])
+        scene.add(resolve[1])
+    }).catch(err => {
+        console.log("添加模型抛错err", err)
+    })
+
+}
+
+//添加环境光
+const addLights = () => {
+    let hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1)
+    scene.add(hemiLight)
+
+    let directionalLight = new THREE.DirectionalLight(0xffffff, 0xffffff, 1)
+    scene.add(directionalLight)
+}
+
 
 //绘制场景
 function renderScene() {
