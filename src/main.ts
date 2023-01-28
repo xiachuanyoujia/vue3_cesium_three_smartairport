8e46fb4147d73e33fc5e7c943e657a61314f3c6f
xiachuanyoujia <1243357202@qq.com>
2023-01-28 19:32:40 +0800

    解决cesium依赖widgets.css导出问题

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import Echarts from "vue-echarts";
 
-import 'cesium/widgets.css'
+import '../public/Widgets/widgets.css'
 import * as Cesium from 'cesium'
 
 createApp(App).use(router).provide("$Cesium", Cesium).component("v-chart", Echarts).mount("#app");
